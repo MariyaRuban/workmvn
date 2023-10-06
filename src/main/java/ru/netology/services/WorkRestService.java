d1b58dae786a71783cc32c6e3d164e176d471105
MRuban <rubanmariyaaleksandrovna@gmail.com>
2023-10-06 20:46:28 +0300

    resources added

--- src/main/java/ru/netology/services/WorkRestService.java
+++ src/main/java/ru/netology/services/WorkRestService.java
@@ -2,7 +2,7 @@ package ru.netology.services;
 
 public class WorkRestService {
     //income доход, expenses расход, threshold остаток
-    public int calcRest (int income, int expenses, int threshold) {
+    public int calcRest(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
         for (int month = 0; month < 12; month++) {
--- src/test/java/ru/netology/services/WorkRestServiceTest.java
+++ src/test/java/ru/netology/services/WorkRestServiceTest.java
@@ -2,27 +2,37 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 //10_000,3_000,20_000
 //100_000,60_000,150_000
 
 public class WorkRestServiceTest {
-    @Test
-        public void testRest () {
+    @ParameterizedTest
+    //  @CsvSource({
+    //        "10000,3000,20000,3",
+    //      "100000,60000,150000,2"
+
+    // } )
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void testRest(int income, int expenses, int threshold, int expected) {
         WorkRestService service = new WorkRestService();
-        int actual = service.calcRest(10_000, 3_000, 20_000);
-        int expected = 3;
+
+        int actual = service.calcRest(income, expenses, threshold);
+        // int expected = 3;
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-        public void testResttt () {
-            WorkRestService service = new WorkRestService();
-            int actual = service.calcRest(100_000, 60_000, 150_000);
-            int expected = 2;
-            Assertions.assertEquals(expected, actual);
+    //   @Test
+    //public void testResttt () {
+    //  WorkRestService service = new WorkRestService();
+    // int actual = service.calcRest(100_000, 60_000, 150_000);
+    // int expected = 2;
+    // Assertions.assertEquals(expected, actual);
 
-        }
+    // }
 
 }
